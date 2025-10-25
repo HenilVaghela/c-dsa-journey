c2a9a2eaa9e6e25f6d18d09d558798200a52a857
HenilVaghela <vaghelahenil7@gmail.com>
2025-10-25 18:59:55 +0530

    linked list traversal in c language
    Implement linked list traversal and node creation with easy and clean code in c language

---
+++ linklist_traversal.c
@@ -0,0 +1,44 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+int data;
+struct Node *next;
+
+};
+
+void linkedlisttraveversal(struct Node *ptr) {
+while(ptr != NULL){
+printf("element %d\n",ptr->data);
+ptr = ptr->next;
+}
+}
+
+int main(){
+
+struct Node *head;
+struct Node *second;
+struct Node *third;
+
+// allocate memory for nodes in the linked list in heap
+head = (struct Node*)malloc(sizeof(struct Node));
+second = (struct Node*)malloc(sizeof(struct Node));
+third = (struct Node*)malloc(sizeof(struct Node));
+
+//link first and second nodes
+head->data = 7;
+head->next = second;
+
+//link second and and third nodes
+second->data = 11;
+second->next = third;
+
+//terminate the list at the third node
+third->data = 66;
+third->next = NULL;
+
+linkedlisttraveversal(head);
+
+return 0;
+}
